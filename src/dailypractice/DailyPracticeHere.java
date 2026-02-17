93db30f311610cb1b7cefc715c1b266ce433e371
Alok Barman <alokb@mac.mynetworksettings.com>
2026-02-16 23:17:51 -0500

    Updated from mac

---
+++ src/dailypractice/DailyPracticeHere.java
@@ -0,0 +1,18 @@
+package dailypractice;
+
+public class DailyPracticeHere {
+
+	public static void main(String[] args) {
+		// 1. Java Program to Count Number of Digits
+		// 2. Java Program To Swap Two Integers
+		// 3. Java Program to Count Number of Even and Odd digits
+		// 4. Java Program Fibonacci Series
+		// 5. Java Program To Reverse An Integer
+		// 6. Java Program to Find Factorial Of A Number
+		// 7. Java Program To Convert Integer To Octal
+		// 8. Java Program To Find Palindrome Number
+		
+		
+	}
+
+}
